c0b998ee7537e5173050c3e6c1328d655a880eed
MrWipper <timy96@neuf.fr>
2014-12-04 19:16:20 +0100

    1e Site

---
+++ site/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Chaton garou : Que le chaton soit avec toi !!!</title>
+		<link rel="stylesheet" href="styles.css" />
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<style type="text/css">
+			html
+			{
+				background-color : black;
+			}
+			img
+			{
+				width: 50%;
+			}
+		</style>
+	</head>
+	<body>
+		<img src="../img/background2.jpg" alt="Chaton garou : Que le chaton soit avec toi !!!"/>
+	</body>
+</html>
