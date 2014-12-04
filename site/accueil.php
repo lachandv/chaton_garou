1a582806b47aeabce501c13b51e02063321b27c1
MrWipper <timy96@neuf.fr>
2014-12-04 23:24:02 +0100

    actu

---
+++ site/accueil.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Chaton garou : Que le chaton soit avec toi !!!</title>
+		<link rel="stylesheet" href="style.css" />
+		<link rel="shortcut icon" href="img/favicon.ico" />
+	</head>
+	<body>
+		<?php include("header.php"); ?>
+	</body>
+</html>
---
+++ site/header.php
@@ -0,0 +1,11 @@
+<header>
+	<div id="logo"></div>
+	<nav>
+		<ul>
+			<li></li>
+		</ul>
+	</nav>
+</header>
+<div id="banniere">
+	<h2></h2>
+</div>
