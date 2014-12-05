e80bb63dba8db5afec854c892589c9e2ca690c99
Poudini-chatons <martilou@etu.univ-savoie.fr>
2014-12-05 04:57:59 +0100

    tests db.php

--- site/db.php
+++ site/db.php
@@ -8,7 +8,7 @@
 }
 try
 {
-	$dbconnexion= new PDO('mysql:host=127.0.0.1;dbname=chaton_garou','chaton_garou','jaimeleslicornes');
+	$dbconnexion= new PDO('mysql:host=127.0.0.1;dbname=chaton_garou','root','nuitinfo');
 }
 catch(Exception $e)
 {
